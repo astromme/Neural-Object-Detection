1954dc92fad269b7ad1546e38bbdf115cf5b1a08
Andrew Stromme <astromme@chatonka.com>
2010-04-10 02:22:39 -0400

    make gngtest more usable

--- gngtest.cpp
+++ gngtest.cpp
@@ -4,11 +4,17 @@
 #include "libgng/imagegenerator.h"
 
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    qDebug() << "Usage: " << argv[0] << " [imagefile]";
+    return 1;
+  }
+  
   GrowingNeuralGas gng = GrowingNeuralGas(5);
 
-  ImageGenerator generator = ImageGenerator("../images/rgb/rgb1.png");
+  ImageGenerator generator = ImageGenerator(argv[1]);
   gng.run(100000, &generator);
   
   QApplication app(argc, argv);
