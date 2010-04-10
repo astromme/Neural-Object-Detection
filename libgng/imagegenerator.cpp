e4346f1baab33fe42c7f4b1c68a76a873d41f72a
Andrew Stromme <astromme@chatonka.com>
2010-04-10 02:18:02 -0400

    remove white background

--- libgng/imagegenerator.cpp
+++ libgng/imagegenerator.cpp
@@ -2,6 +2,7 @@
 #include "imagegenerator.h"
 
 #include <QDebug>
+#include <QList>
 
 ImageGenerator::ImageGenerator(const QString& filePath)
   : PointGenerator(),
@@ -20,15 +21,31 @@ int ImageGenerator::dimension()
 
 Point ImageGenerator::generatePoint()
 {
-  int x = qrand() % width();
-  int y = qrand() % height();
+  int x, y;
+  QRgb colors;
+  bool backgroundPoint = true;
   
+  QList<int> backgroundColor;
+  backgroundColor << 255 << 255 << 255;
+  
+  // rand points until one isn't a background point
+  while (backgroundPoint) {
+    x = qrand() % width();
+    y = qrand() % height();
+    colors = m_image.pixel(x, y);
+    
+    if ((abs(qRed(colors) - backgroundColor[0]) > 50)
+     || (abs(qGreen(colors) - backgroundColor[1]) > 50)
+     || (abs(qBlue(colors) - backgroundColor[2]) > 50)) {
+      backgroundPoint = false;
+    }
+  }
+   
   Point p;
   p.resize(5);
   p[0] = normalize(x, width());
   p[1] = normalize(y, height());
   
-  QRgb colors = m_image.pixel(x, y);
   p[2] = normalize(qRed(colors), 255);
   p[3] = normalize(qGreen(colors), 255);
   p[4] = normalize(qBlue(colors), 255);
