ff376e93e9601e8fac1f46db7ec6e0af04a59522
Andrew Stromme <astromme@chatonka.com>
2010-04-11 13:52:53 -0400

    fix passing in an image file

--- gngtest.cpp
+++ gngtest.cpp
@@ -9,45 +9,26 @@
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
-    qDebug() << "Usage: " << argv[0] << " [frame]";
+    qDebug() << "Usage:" << argv[0] << "[image]";
     return 1;
   }
   
-  //QString imagePath = argv[1];
+  QString imagePath = argv[1];
+  
   QApplication app(argc, argv);
-  
-    
-   QString templateImagePath = "../images/rgb/rgb%1.png";
-
   GrowingNeuralGas gng(5, -1, 1, 5000);
-  QList<ImageGenerator> generators;
-  QList<QString> imagePaths;
-  
-  for(int i=1; i<=11; i++) {
-    QString imagePath = templateImagePath.arg(i);
-    generators.append(ImageGenerator(imagePath));
-    imagePaths.append(imagePath);
-  }
-  
-
-  generators.append(ImageGenerator("../images/laptop-desk.jpg"));
-  imagePaths.append("../images/laptop-desk.jpg");
-  
-  int frame = QString(argv[1]).toInt();  
-  
-  GngViewer view(generators[frame].width(), generators[frame].height());
+  ImageGenerator generator(imagePath);
+  GngViewer view(generator.width(), generator.height());
 
   view.setGng(&gng);
   view.show();
   
   QObject::connect(&gng, SIGNAL(updated()), &view, SLOT(update()));
   
-  
-  
   QPixmap img;
-  img.load(imagePaths[frame]);
+  img.load(imagePath);
   view.setSource(img);
-  gng.setPointGenerator(&generators[frame]);
+  gng.setPointGenerator(&generator);
   
   gng.run(100000);
   app.exec();
