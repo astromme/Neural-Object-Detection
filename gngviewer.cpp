1861ab73910bc2fc11cdfda513010714229fcf73
Andrew Stromme <astromme@chatonka.com>
2010-04-13 22:05:19 -0400

    comment out focusing code

--- gngviewer.cpp
+++ gngviewer.cpp
@@ -97,7 +97,7 @@ void GngViewer::paintEvent(QPaintEvent* e)
     painter.setBrush(transparentGray);
     int x = unNormalize(focus[0], m_width) - 0.05*width();
     int y = unNormalize(focus[1], m_height) - 0.05*height();
-    painter.drawEllipse(x, y, 0.1*width(), 0.1*height());
+    painter.drawEllipse(x, y, 0.1*width(), 0.1*height()); //TODO Don't hardcode
   }
   
   // Draw framecount
--- libgng/gng.cpp
+++ libgng/gng.cpp
@@ -287,15 +287,16 @@ void GrowingNeuralGas::step(const Point& trainingPoint)
     insertNode();
   }
   
+  // TODO Make less hacky
   //qDebug() << "Errors: First:" << winners.first->error() << " and Second:" << winners.second->error();
-  qreal errorThreshold = 10*m_targetError;
-  if (m_pickCloseToCountdown > 0) {
-    m_pickCloseToCountdown--;
-  } else if ((averageError() < m_targetError) && (winners.second->error() > errorThreshold)) {
-    //qDebug() << "Picking close to " << trainingPoint;
-    m_pickCloseTo = trainingPoint;
-    m_pickCloseToCountdown = 500;
-  }
+//   qreal errorThreshold = 10*m_targetError;
+//   if (m_pickCloseToCountdown > 0) {
+//     m_pickCloseToCountdown--;
+//   } else if ((averageError() < m_targetError) && (winners.second->error() > errorThreshold)) {
+//     //qDebug() << "Picking close to " << trainingPoint;
+//     m_pickCloseTo = trainingPoint;
+//     m_pickCloseToCountdown = 500;
+//   }
   
   reduceAllErrors();
   m_stepCount++;
@@ -340,11 +341,11 @@ void GrowingNeuralGas::run()
     }
     
     Point nextPoint;
-    if (false && m_pickCloseToCountdown > 0) {
-      nextPoint = m_pointGenerator->generateNearbyPoint(m_pickCloseTo);
-    } else {
+//     if (false && m_pickCloseToCountdown > 0) {
+//       nextPoint = m_pointGenerator->generateNearbyPoint(m_pickCloseTo);
+//     } else {
       nextPoint = m_pointGenerator->generatePoint();
-    }
+//     }
     step(nextPoint);
     
     m_dataAccess->unlock();
