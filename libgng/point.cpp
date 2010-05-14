18f8632bb157deba330ccb69e50fdd7146945ab5
Andrew Stromme <astromme@chatonka.com>
2010-05-14 16:38:31 -0500

    wrap around point distance

--- libgng/point.cpp
+++ libgng/point.cpp
@@ -19,11 +19,28 @@ Point::Point(int dimension)
 qreal Point::distanceTo(const Point &other) const {
   qreal dist = 0;
   
+  // Color wraps around. HACK: Specific to HSL/HSV
+  bool otherIsSmaller = other[2] < at(2) ? true : false;
+  if (otherIsSmaller) {
+    // Calculate distance both ways and take shorter amount.
+    qreal clockwise = at(2) - other[2];
+    qreal counterclockwise = other[2] - at(2) + 1;
+    dist += pow(qMin(clockwise, counterclockwise), 2);
+  } else {
+    qreal clockwise = other[2] - at(2);
+    qreal counterclockwise = at(2) - other[2] + 1;
+    dist += pow(qMin(clockwise, counterclockwise), 2);
+  }
+  
+  // Calculate rest of distances
   for (int i=0; i<size(); i++) {
+    if (i == 2) {
+      continue;
+    }
     dist += pow(other[i]-at(i), 2);
   }
   
-  return sqrt(dist) + 1.5*xyDistanceTo(other);
+  return sqrt(dist) + 2*xyDistanceTo(other);
   //return dist;
 }
 
